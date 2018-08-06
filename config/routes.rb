cbab80f039cb71f664b28691326579d1f1743902
Ertemelle <phenixime@hotmail.fr>
2018-08-06 16:38:22 +0200

    modif navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'sessions/new'
   root to:'static_pages#home', as: 'home'
   get '/club', to: 'static_pages#club', as: 'club'
-  resources :users
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users
 end
