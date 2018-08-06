f8608794aaeedeb8da43031eb31b2f1a4fec5a68
Romain <romainsaillour@orange.fr>
2018-08-06 20:45:16 +0200

    test email en double, paramètre manquant

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,29 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
-    assert_response :success
+  test "email already user" do
+    get '/users/new'
+    post '/users', params: {user: {email: "romano@mail.com"}}
+      assert flash[:danger], "Email already used"
   end
-
-  test "should get create" do
-    get users_create_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get users_edit_url
-    assert_response :success
-  end
-
-  test "should get delete" do
-    get users_delete_url
-    assert_response :success
-  end
+    test "missing param" do
+      get '/users/new'
+      post '/users', params:{user:{first_name: "lal", last_name: "lolo ", email: "lili"}}
+        assert flash[:danger], "Empty params"
+    end
 
 end
