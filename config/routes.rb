007585350d601a2e04a6254b28ba0715f1be2449
Ertemelle <phenixime@hotmail.fr>
2018-08-06 12:10:06 +0200

    modif routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/delete'
+  root to:'static_pages#home' as: 'home'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
