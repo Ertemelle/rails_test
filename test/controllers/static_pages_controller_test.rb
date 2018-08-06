6d402f9d8c2e04f6f323211174fcf81b473b7081
Ertemelle <phenixime@hotmail.fr>
2018-08-06 21:31:20 +0200

    on est fatigués, on a faim, bisou

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,55 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get home_path
+  def setup
+  	@user = users(:bob)
+  end
+
+  test 'Get to homepage' do
+    get '/'
     assert_response :success
+  end
+
+  test 'Assert the presence of login links when logged out' do
+    get '/'
+    assert_select "a[href=?]", club_path, count: 0
+    assert_select "a[href=?]", login_path, count: 2
+  end
+
+  test 'Assert the presence of club link when logged in' do
+    get login_path
+    post login_path, params: { session: { email: @user.email  } }
+    follow_redirect!
+    get '/'
+    assert_select "a[href=?]", club_path, count: 2
+    assert_select "a[href=?]", login_path, count: 0
+  end
+
+  test 'Assert redirection to login when logged_out' do
+    get '/club'
+    assert_redirected_to 'sessions#new'
+    follow_redirect!
+  end
+
+  test 'Assert the presence of profile link when logged in' do
+    get login_path
+    post login_path, params: { session: { email: @user.email  } }
+    follow_redirect!
+    get '/'
+    assert_select "a[href=?]", user_path(@user), count: 1
+  end
+
+  test 'Show info of the user after logged in' do
+    get login_path
+    post login_path, params: { session: { email: @user.email  } }
+    get user_path(@user)
+    assert_select "h1", "Welcome #{@user.first_name}"
+  end
+
+  test 'Assert possibility to access club page when logged_in' do
+    get login_path
+    post login_path, params: { session: { email: @user.email  } }
+    assert_redirected_to club_path
+    follow_redirect!
   end
 end
