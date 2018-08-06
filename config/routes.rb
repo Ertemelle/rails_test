c37c7d264661df817509a7d4ec31d4f6510b6f2e
Ertemelle <phenixime@hotmail.fr>
2018-08-06 11:59:41 +0200

    ajout des models+controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
