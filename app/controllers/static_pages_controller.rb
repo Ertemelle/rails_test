94e056999b445aea8adde2369fbebc22f0f67d3d
Romain <romainsaillour@orange.fr>
2018-08-06 14:48:27 +0200

    logged in ok

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,8 @@
 class StaticPagesController < ApplicationController
-  before_action :require_login
+  #before_action :require_login
   def home
   end
 
-  def require_login
-  	if logged_in?
-  	else
-  		redirect_to login_path
-  	end
-  end
+
+
 end
