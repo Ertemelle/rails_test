82c03077efdadcac47fd1e8622f8304c3bb2c8d0
breizzh <remisamsoninvestisseur@gmail.com>
2018-08-06 15:39:23 +0200

    page du club créée

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-
-
+  def club
+		@users = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   root to:'static_pages#home', as: 'home'
+  get '/club', to: 'static_pages#club', as: 'club'
   resources :users
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
