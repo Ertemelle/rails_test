b12d3b7225e5275b95c4b7be3b944f9c7a5d59f0
Ertemelle <phenixime@hotmail.fr>
2018-08-06 17:19:11 +0200

    test users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = User.new(first_name: "Lala", last_name: "Lolo", email: "user@example.com")
+	end
+
+	test "email address should be unique" do
+		duplicate_user = @user.dup
+		@user.save
+		assert_not duplicate_user.valid?
+	end
+
+	test "first_name should be present" do
+		@user.first_name = "        "
+		assert_not @user.valid?
+	end
+
+	test "last_name should be present" do
+		@user.last_name = "         "
+		assert_not @user.valid?
+	end
+
+	test "email should be present" do
+		@user.email = "             "
+		assert_not @user.valid?
+	end
 end
